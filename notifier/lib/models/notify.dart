0901cc003b76375046f68d248166bd13193c83f6
1stWorldMaster <pranaythapakoffical@gmail.com>
2025-03-12 10:47:44 +0530

    Initial Commit

---
+++ notifier/lib/models/notify.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Notify {
+  DateTime? date;
+  TimeOfDay? time;
+  String? title;
+  String? description;
+
+  Notify({this.date, this.time, this.title, this.description});
+}
